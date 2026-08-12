b2c42b081f9e1f8d5b23a14aa9ba4cbc6a0bbc53
moto-taka <pspdswii3@gmail.com>
2026-08-12 14:55:07 +0900

    gloop 0.4.0

--- Formula/gloop.rb
+++ Formula/gloop.rb
@@ -1,8 +1,8 @@
 class Gloop < Formula
   desc "Foreground Rust CLI for configurable multi-provider agent and command graphs"
   homepage "https://github.com/moto-taka/gloop"
-  url "https://github.com/moto-taka/gloop/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "4ad58cb167b1250b22e5d1a46efebcd9569767199c9d8431681b9310d1c0ce67"
+  url "https://github.com/moto-taka/gloop/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "968cf61930c34bf4b1d53a5a8ab99028d7f46a38f27e74ecda90a0dd43ad3938"
   license "Apache-2.0"
 
   depends_on "rust" => :build
